57fc2fcbc8dff6707478b5e0fd13f1553f3e5daa
lizhicheng <245401566@qq.com>
2020-01-11 21:24:54 +0800

    添加对应解答

--- filter注意事项.js
+++ filter注意事项.js
@@ -1,6 +1,29 @@
 let nums = [1, 2, 3];
-nums.length = 10;
+nums[10] = 10;
+console.log(nums);
 console.log(nums.filter(x => x === undefined))
 
 // A. [undefined*7]
 // B. []    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 直接设定数组length并不会被es5的几个包括(Array.prototype.forEach,Array.prototype.map,Array.prototype.filter,Array.prototype.reduce，。。)数组迭代方法循环。
+// 之前我也不理解，但是在MDN看到它们的Polyfill，迭代的同时都通过 in 运算符 判断当前迭代的索引是否在数组中。
+// 而直接设定长度没有设值的数组索引直接因为不能通过这个判断被剔除了。
+// 其余元素都是空元素 并不是undefined filter map等方法会直接跳过空元素  empty
--- 作用域.js
+++ 作用域.js
@@ -1,9 +1,57 @@
-var name = '1';
+
+var name = 'World!';
 (function () {
   if (typeof name === 'undefined') {
-    var name = 2;
-    console.log('hello1-' + name);
+    var name = 'Jack';
+    console.log('Goodbye ' + name);
   } else {
-    console.log('hello2-' + name);
+    console.log('Hello ' + name);
   }
-})()
+})();
+
+// 输出什么？为什么？
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 变量声明提升的作用
+// 在一个函数体内声明的变量，JS解析器都会将其移动到函数体的顶部
+
+// var name = 'World!';
+// (function () {
+//     if (typeof name === 'undefined') {
+//         var name = 'Jack';
+//         console.log('Goodbye ' + name);
+//     } else {
+//         console.log('Hello ' + name);
+//     }
+// })();
+// 等价为==>
+
+// var name = 'World!';
+// (function () {
+//     var name;    
+//     if (typeof name === 'undefined') {
+//         name = 'Jack';
+//         console.log('Goodbye ' + name);
+//     } else {
+//         console.log('Hello ' + name);
+//     }
+// })();
+// 执行的时候有个变量查找的过程，如果在当前函数体内没找到，就会到定义的函数体的外层函数中去寻找，一直向上到全局对象中寻找，还是找不到就会报TypeError错误
+
